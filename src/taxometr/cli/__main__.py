809e865eb679071e3d628e95f20ad40e8059a2d1
atribolt <atribolt@gmail.com>
2025-01-24 20:27:55 +0300

    CLI на HTTP #3
    - начал перевод CLI на работу по HTTP протоколу

--- src/taxometr/cli/__main__.py
+++ src/taxometr/cli/__main__.py
@@ -1,4 +1,24 @@
-from taxometr.cli import cli
+import click
+import taxometr.cli as tcli
+
+
+@click.group()
+@click.option('-v', '--verbose', count=True,
+              default=False, help='Enable verbose output (-v, -vv, -vvv)')
+@click.option('-C', '--colored', is_flag=True, default=False, help='Enable colored output')
+@click.option('-c', '--config', 'config_file', type=ReadableFile, help='Config file')
+@context
+def cli(ctx, verbose, colored, config_file):
+  """CLI for taxometr"""
+  printing.init(verbose, colored)
+  config.load_from_file(config_file)
+
+  ctx.database = get_connection()
+  ctx.database.bind(TABLES)
+
+
+cli.add_command(task_group, 'task')
+cli.add_command(action_group, 'action')
 
 
 if __name__ == '__main__':
